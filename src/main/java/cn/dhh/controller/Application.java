757a3344f4ec461202b20d9c825c8c69f760bed2
duhuanhuan <duhuanhuan@vcredit.com>
2019-08-26 16:43:56 +0800

    初始化项目

---
+++ src/main/java/cn/dhh/controller/Application.java
@@ -0,0 +1,17 @@
+package cn.dhh.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description
+ * Date 2019/8/26 16:30
+ *
+ * @author duhuanhuan
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
