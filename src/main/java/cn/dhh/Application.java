47d3855b8257d74b8bddc42042692252ad115084
duhuanhuan <duhuanhuan_t@163>
2019-10-14 15:23:02 +0800

    b02

--- src/main/java/cn/dhh/Application.java
+++ src/main/java/cn/dhh/Application.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     //b
+    //b2
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
